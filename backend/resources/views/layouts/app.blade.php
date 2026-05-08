dff4e54743a969ac7469859de6630198b2e7f72d
DeXon (Oskar) <dexon18@gmail.com>
2026-05-08 13:58:18 +0200

    feat(ui): add corporate favicon and update layouts

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'DX License Manager') }} - @yield('title', 'Portal')</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
 
     <!-- Theme Initialization -->
     <script>
